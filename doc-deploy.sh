bd7ae74971eec2898d95470097f0673ffdeabc12
Dan Korostelev <nadako@gmail.com>
2016-09-02 03:07:06 +0300

    fix?

--- doc-deploy.sh
+++ doc-deploy.sh
@@ -35,7 +35,7 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+if [ $(git status --porcelain | wc -l) -lt 1 ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
