bbcffa813a345d7e9c8f78a71485cc41eb52ed59
Jens Fischer <jensfischer95@gmail.com>
2019-05-24 00:20:00 +0200

    Rename the example's HelloHaxe.hx to Main.hx
    Makes it more useful for using it as a template for a new extension.

--- example/src/Main.hx
+++ example/src/Main.hx
@@ -1,6 +1,8 @@
-class HelloHaxe {
+import vscode.*;
+
+class Main {
 	@:expose("activate")
-	static function activate(context:vscode.ExtensionContext) {
+	static function activate(context:ExtensionContext) {
 		context.subscriptions.push(Vscode.commands.registerCommand("hellohaxe.sayHello", function() {
 			Vscode.window.showInformationMessage("Hello from Haxe!");
 		}));
