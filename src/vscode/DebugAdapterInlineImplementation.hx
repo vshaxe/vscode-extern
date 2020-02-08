2cebf0ad56f0f6e2ab9faf2649e5f8dd0a59daad
Jens Fischer <jensfischer95@gmail.com>
2020-02-08 18:56:03 +0100

    Add missing `@:jsRequire`

--- src/vscode/DebugAdapterInlineImplementation.hx
+++ src/vscode/DebugAdapterInlineImplementation.hx
@@ -3,6 +3,7 @@ package vscode;
 /**
  * A debug adapter descriptor for an inline implementation.
  */
+@:jsRequire("vscode", "DebugAdapterInlineImplementation")
 extern class DebugAdapterInlineImplementation {
 	/**
 	 * Create a descriptor for an inline implementation of a debug adapter.
